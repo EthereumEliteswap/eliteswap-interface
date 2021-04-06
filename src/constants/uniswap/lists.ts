e04abc2f20c03894f532567a053601cd0d87aa68
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2021-04-06 18:38:59 +0300

    Update token lists

--- src/constants/uniswap/lists.ts
+++ src/constants/uniswap/lists.ts
@@ -1,21 +1,42 @@
+const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
+const AAVE_LIST = 'tokenlist.aave.eth'
+const SYNTHETIX_LIST = 'synths.snx.eth'
+const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
+const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
+const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
+const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
+const CMC_ALL_LIST = 'defi.cmc.eth'
+const CMC_STABLECOIN = 'stablecoin.cmc.eth'
+const KLEROS_LIST = 't2crtokens.eth'
+const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
+const UNISWAP_LIST = 'tokens.uniswap.eth'
+const DHARMA_LIST = 'tokenlist.dharma.eth'
+const ELITESWAP_LIST = 'https://unpkg.com/@eliteswap/default-token-list@1.6.1'
+
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = GEMINI_LIST
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://unpkg.com/@eliteswap/default-token-list@1.6.3', // eliteswap
-  't2crtokens.eth', // kleros
+  ELITESWAP_LIST,
+  UNISWAP_LIST,
+  KLEROS_LIST,
   'tokens.1inch.eth', // 1inch
-  'synths.snx.eth',
-  'tokenlist.dharma.eth',
-  'defi.cmc.eth',
+  SYNTHETIX_LIST,
+  DHARMA_LIST,
+  CMC_ALL_LIST,
   'erc20.cmc.eth',
-  'stablecoin.cmc.eth',
+  CMC_STABLECOIN,
   'tokenlist.zerion.eth',
-  'tokenlist.aave.eth',
-  'https://tokens.coingecko.com/uniswap/all.json',
-  'https://app.tryroll.com/tokens.json',
-  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  AAVE_LIST,
+  COINGECKO_LIST,
+  ROLL_LIST,
+  COMPOUND_LIST,
   'https://defiprime.com/defiprime.tokenlist.json',
-  'https://umaproject.org/uma.tokenlist.json'
+  UMA_LIST,
+  WRAPPED_LIST,
+  SET_LIST,
+  OPYN_LIST
 ]
