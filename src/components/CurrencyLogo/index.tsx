4e6303612d4cd93c20df674057713af447ecf8f8
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2021-04-09 11:38:20 +0300

    Fix BNB logo

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -2,7 +2,7 @@ import { Currency, ETHER, Token } from '@eliteswap/sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-import EthereumLogo from '../../assets/images/ethereum-logo.png'
+import BNBLogo from '../../assets/images/BNB_logo.png'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
@@ -10,7 +10,7 @@ import Logo from '../Logo'
 const getTokenLogoURL = (address: string) =>
   `https://exchange.pancakeswap.finance/images/coins/${address}.png`
 
-const StyledEthereumLogo = styled.img<{ size: string }>`
+const StyledBNBLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
@@ -49,7 +49,7 @@ export default function CurrencyLogo({
   }, [currency, uriLocations])
 
   if (currency === ETHER) {
-    return <StyledEthereumLogo src={EthereumLogo} size={size} style={style} />
+    return <StyledBNBLogo src={BNBLogo} size={size} style={style} />
   }
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
